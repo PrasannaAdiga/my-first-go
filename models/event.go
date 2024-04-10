4450b88a5a8193229af3b73f9db66dc4e0380305
Prasanna Adiga <prasanna@Prasannas-MacBook-Air.local>
2024-04-10 10:59:04 +0530

    Saving and fetching data from DB

--- models/event.go
+++ models/event.go
@@ -1,23 +1,58 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/PrasannaAdiga/my-first-go/db"
+)
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
 	DateTime    time.Time `binding:"required"`
-	UserId      int
+	UserId      int64
 }
 
-var events = []Event{}
-
-func (e Event) Save() {
+func (e Event) Save() error {
 	// Add it to database
-	events = append(events, e)
+	query := `
+	INSERT INTO events(name, description, location, dateTime, user_id) 
+	VALUES (?, ?, ?, ?, ?)`
+
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	res, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserId)
+	if err != nil {
+		return err
+	}
+	id, err := res.LastInsertId()
+	e.ID = id
+	return err
 }
 
-func GetAllEvents() []Event {
-	return events
+func GetAllEvents() ([]Event, error) {
+	query := "SELECT * FROM events"
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var events []Event
+
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserId)
+		if err != nil {
+			return nil, err
+		}
+		events = append(events, event)
+	}
+	return events, nil
 }
