a47697ca677f46e112bc9d337cba46d6de35522e
Prasanna Adiga <prasanna@Prasannas-MacBook-Air.local>
2024-05-04 16:11:34 +0530

    Added authentication middleware

--- routes/events.go
+++ routes/events.go
@@ -5,7 +5,6 @@ import (
 	"strconv"
 
 	"github.com/PrasannaAdiga/my-first-go/models"
-	"github.com/PrasannaAdiga/my-first-go/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -36,26 +35,15 @@ func getEvent(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
-	token := context.Request.Header.Get("Authorization")
-	if token == "" {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
-		return
-	}
-	userId, err := utils.VerifyToken(token)
-	if err != nil {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
-		return
-	}
-
 	var event models.Event
-	err = context.ShouldBindJSON(&event)
+	err := context.ShouldBindJSON(&event)
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
 		return
 	}
 
-	event.UserId = userId
+	event.UserId = context.GetInt64("userId")
 
 	err = event.Save()
 
